d04ceb61847cff55b67a445159b3edac45fb6bf8
Tom Hunt <tomdicksonhunt@gmail.com>
2012-03-17 13:39:45 -0600

    The launcherinfo file is only useful transitively for builds; no need to track it.

--- launcherinfo.py
+++
@@ -1,8 +0,0 @@
-documentation_extensions = 'txt html'
-build_mac = True
-build_all = False
-build_windows = True
-distribution_base = 'KBDH'
-ignore_extensions = '~ .bak'
-build_linux = True
-executable_name = 'KBDH'
