4321c1cfb5ab1f4f56e2542dc1ee5963acca5951
Agasa <megamanx20042002@yahoo.it>
2012-03-17 11:50:40 +0100

    Configured for building.
    Signed-off-by: Agasa <megamanx20042002@yahoo.it>

--- launcherinfo.py
+++ launcherinfo.py
@@ -1,8 +1,8 @@
-executable_name = 'KBDH'
 documentation_extensions = 'txt html'
 build_mac = True
+build_all = False
 build_windows = True
 distribution_base = 'KBDH'
 ignore_extensions = '~ .bak'
 build_linux = True
-build_all = False
+executable_name = 'KBDH'
